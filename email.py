677d7eaf6c5fa1f68af3e2036207aae9402b6a00
Darren Homrighausen <dhomrigh@gmail.com>
2018-12-13 10:35:45 -0600

    Update email.py

--- email.py
+++ email.py
@@ -5,7 +5,8 @@ I'm not in the habit of giving robots my email address.  Are you a robot? Answer
 robotStatus = raw_input(prompt)
 
 if robotStatus == 'no':
-  robotStatus = str.replace(robotStatus, 'o','@')
-  print 'dhomrighause'+robotStatus+'smu.edu'
+  robotStatus = str.replace(robotStatus, 'n','m')
+  robotStatusReverse = robotStatus[::-1]
+  print 'dh'+robotStatusReverse+'righ'+'@'+'gmail.c'+robotStatusReverse
 else:
   print "either you're a robot or did't follow directions.  Try again only if you're not a robot"
